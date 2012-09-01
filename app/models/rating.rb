501d82d6daf80a15389ca91edcf29b3d596b3219
Daniel Spaude <daniel@spaude.de>
2012-09-02 01:44:13 +0200

    fix rating-error

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,103 +1,107 @@
 class Rating
-require 'linearregression.rb'
+  require 'linearregression.rb'
 
 
-def all
-	list = Array.new
-	i = 0
-	ServicePartner.all.each do |service_provider| 
-		list[i] =	getAvgRating(service_provider)
-		i += 1
-	end
-	list
+  def initialize
+
+    weights = Array.new([1,0.95,0.9,0.85,0.8,0.75,0.7,0.65,0.6,0.55,0.5,0.4,0.3,0.2,0.1])
+    @reg = LinearRegression.new(weights)
+    @log = Logger.new("/home/daniel/Desktop/transfair.log")
+
+  end
+
+  def all
+    list = Array.new
+    i = 0
+    ServicePartner.all.each do |service_provider|
+      list[i] =	getAvgRating(service_provider)
+      i += 1
+    end
+    list
+  end
+
+
+
+  def getAvgRating(service_provider)
+    if (service_provider.employments.count > 0)
+      a = 0.5
+      b = 0.5
+      r = ((a * getClientRating(service_provider)) + (b * getSpotCheckerRating(service_provider))) / (a+b)
+    else
+      nil
+    end
+  end
+
+  # ------------------------
+
+  private
+  def getSpotCheckerRating(service_provider)
+    ratings = Array.new
+    service_provider.employments.each do |employment|
+      ratings << employment.rating_servicepartner
+    end
+    getRating(ratings)
+
+  end
+
+
+
+  def getClientRating(service_provider)
+    ratings = Array.new
+    if (service_provider.employments)
+      service_provider.employments.each do |employment|
+        ratings << employment.job.rating_client
+      end
+      getRating(ratings)
+    end
+  end
+
+  def getRatingSimple(ratings)
+    ratings = ratings.sort
+    i = 0
+    r = 0
+    ratings.each do |rating|
+      r += rating
+      i = i + 1
+    end
+    if (ratings.count != 0)
+      avg = r.to_f / ratings.count
+    else
+      0
+    end
+  end
+
+
+  def predict(x)
+    if (@reg)
+      y = @reg.predict(x)
+    else
+      y = 0
+    end
+
+    @log.debug "--------" + y.to_s
+  end
+
+
+  def getRating(ratings)
+
+    ratings = ratings.sort
+    i = 0
+    r = 0
+    sum_weight = 0
+    ratings.each do |rating|
+      weight = @reg.predict(i)
+      @log.debug("--------" + weight.to_s)
+      r += rating * weight
+      sum_weight += weight
+      i = i + 1
+    end
+    if (ratings.count != 0)
+      avg = r.to_f / sum_weight
+    else
+      0
+    end
+  end
+
+
 end
-
-
-
-def getAvgRating(service_provider)
-	if (service_provider.employments.count > 0)
-		a = 0.5
-		b = 0.5	
-		r = ((a * getClientRating(service_provider)) + (b * getSpotCheckerRating(service_provider))) / (a+b)
-	else
-		nil
-	end
-end
-
-# ------------------------
-
-private
-def getSpotCheckerRating(service_provider)
-		ratings = Array.new
-		service_provider.employments.each do |employment|
-			ratings << employment.rating_servicepartner
-		end
-		getRating(ratings)
-
-end
-
-
-
-def getClientRating(service_provider)
-		ratings = Array.new
-		if (service_provider.employments)
-			service_provider.employments.each do |employment|
-				ratings << employment.job.rating_client
-			end
-			getRating(ratings)
-		end
-end
-
-def getRatingSimple(ratings)
-	ratings = ratings.sort
-		i = 0
-		r = 0
-		ratings.each do |rating|
-			r += rating
-			i = i + 1
-		end
-		if (ratings.count != 0)
-			avg = r.to_f / ratings.count
-		else
-			0
-		end
-end
-
-
-def predict(x)
-	if (@reg)
-		y = @reg.predict(x)		
-	else
-		y = 0
-	end
-
-	@log.debug "--------" + y.to_s
-end
-
-@log = Logger.new("/home/ichaib/transfair.log")	
-		
-def getRating(ratings)
-		weights = Array.new([1,0.95,0.9,0.85,0.8,0.75,0.7,0.65,0.6,0.55,0.5,0.4,0.3,0.2,0.1])
-		reg = LinearRegression.new(weights)
-		
-		ratings = ratings.sort
-		i = 0
-		r = 0
-		ratings.each do |rating|
-			weight = reg.predict(i)
-			@log.debug "--------" + weight.to_s 
-			r += rating * weight
-			sum_weight += weight
-			i = i + 1
-		end
-		if (ratings.count != 0)
-			avg = r.to_f / sum_weight
-		else
-			0
-		end
-end
-
-
-end
-
-
