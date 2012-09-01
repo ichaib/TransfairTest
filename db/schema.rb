746ed97e44418450fb8227f8762b61498ed91749
Daniel Spaude <daniel@spaude.de>
2012-09-01 20:44:16 +0200

    run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120901165706) do
+ActiveRecord::Schema.define(:version => 20120901173505) do
 
   create_table "employments", :force => true do |t|
     t.integer  "rating_servicepartner"
@@ -25,6 +25,11 @@ ActiveRecord::Schema.define(:version => 20120901165706) do
     t.integer  "rating_client"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+  end
+
+  create_table "ratings", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "service_partners", :force => true do |t|
